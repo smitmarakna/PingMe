0587ec34657af36e424e77b12fe69b68be0e2b26
smitmarakna <smitmarakna1709@gmail.com>
2025-06-21 14:12:06 +0530

    another caps mistake

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useChatStore } from "../store/useChatStore";
-import SidebarSkeleton from "./skeletons/sidebarSkeleton";
+import SidebarSkeleton from "./skeletons/SidebarSkeleton";
 import { Users } from "lucide-react";
 import { useAuthStore } from "../store/useAuthStore";
 const Sidebar = () => {

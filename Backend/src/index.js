8b02b5a65926890df3fd881493b056d69652faf5
smitmarakna <smitmarakna1709@gmail.com>
2025-06-21 13:00:55 +0530

    env change

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: "20mb" }));
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV !== "development") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("*", (req, res) => {

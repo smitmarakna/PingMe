e7883ae749cb30afbdc700c42826eba8637f7143
smitmarakna <smitmarakna1709@gmail.com>
2025-06-21 13:23:58 +0530

    deployment issue

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import authRoutes from './routes/auth.route.js';
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import dotenv from 'dotenv';
-import { connectDB } from './lib/db.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import path from 'path';
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
 dotenv.config();
 
 const app = express();
@@ -16,18 +16,18 @@ app.use(express.json({ limit: "20mb" }));
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-if (process.env.NODE_ENV !== "development") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+	});
 }
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
+	console.log(`Server is running on port ${PORT}`);
+	connectDB();
 });

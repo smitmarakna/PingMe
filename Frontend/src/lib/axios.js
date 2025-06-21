990aed85a3c958282ff120538619076efde193c4
smitmarakna <smitmarakna1709@gmail.com>
2025-06-21 14:33:40 +0530

    port changes

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === 'development' ? 'http://localhost:3000/api' : '/api',
+    baseURL: import.meta.env.MODE === 'development' ? 'http://localhost:5003/api' : '/api',
     withCredentials: true,
 });
 
--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import axiosInstance from "../lib/axios";
 import toast from "react-hot-toast";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5003/api" : "/";
 
 export const useAuthStore = create((set) => ({
 	authUser: null,
